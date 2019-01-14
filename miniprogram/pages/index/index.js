ef01aeb3d7f7df237a9a797367c9b1afa41319a3
Jing <274744105@qq.com>
2019-01-15 07:26:19 +0800

    Add files via upload

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,9 @@
+Page({
+  data: {
+    hotMovieImage: "/images/p480747492.jpg",
+    hotMovieTitle:"肖申克的救赎",
+    recommender:"彩色变调夹",
+    recommendMessage: "给你推荐了一部电影",
+    user_head:"/images/tabBar_hot.png"
+  }
+})
